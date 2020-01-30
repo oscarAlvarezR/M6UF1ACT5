4f5fa6c0e1af0da6b80b77809f9f9826f340210b
Alumne <Alumne@104-PC13>
2020-01-30 17:08:13 +0100

    segon

--- src/UF1ACT5.java
+++ src/UF1ACT5.java
@@ -1,11 +1,19 @@
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream.GetField;
 import java.util.Scanner;
 
+import javax.sql.rowset.spi.XmlWriter;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -27,39 +35,36 @@ public class UF1ACT5 {
 			Document document = documentBuilder.parse(archivo);
 			document.getDocumentElement().normalize();
 
+			int contadorId = 1;
 			NodeList listaAlumnos = document.getDocumentElement().getChildNodes();
 
 			for (int i = 0; i < listaAlumnos.getLength(); i++) {
 				Node node = listaAlumnos.item(i);
 
 				if (node.getNodeType() == Node.ELEMENT_NODE) {
-					
+
 					Element eElement = (Element) node;
-					
+
 					if (node.hasChildNodes()) {
-						
+
 						if (node.getAttributes().getNamedItem("id") != null){
 
 							Element e = (Element) node;
 							e.setIdAttribute("id", true);
+							contadorId++;
 						}
 					}
-
-					//if(eElement.hasChildNodes()) {
-						//NodeList nl = node.getChildNodes();
-						//recursivaSenseMostrar(nl);
-					//}
 				}
 			}
 
 			boolean parar = false;
-			
+
 			while (!parar) {
-				
+
 				System.out.println("<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>");
 				System.out.println("0 Salir del programa");
 				System.out.println("1 Mostrar XML:");
-				System.out.println("2 Situarse en element XML a partir de la id:");
+				System.out.println("2 Modificar XML:");
 				int seleccio = teclado.nextInt();
 
 				if (seleccio == 1) {
@@ -102,37 +107,80 @@ public class UF1ACT5 {
 					//				DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
 					//				Document document = documentBuilder.parse(archivo);
 
-					System.out.println("Introdueix la ID:");
-					int seleccioId = teclado.nextInt();
 
-					Element eElement = document.getElementById(String.valueOf(seleccioId));
-					
 					System.out.println("1 Editar Elements");
 					System.out.println("2 Editar Atributs");
 					int seleccioEdicio = teclado.nextInt();
-					
+
 					if (seleccioEdicio == 1) {
-						
+
 						System.out.println("1 Crear Element");
 						System.out.println("2 Modificar Element");
 						System.out.println("3 Eliminar Element");
 						seleccioEdicio = teclado.nextInt();
-						
+						teclado.nextLine();
+
 						if (seleccioEdicio == 1) {
-							
-							eElement = document.createElement("nouElement");
-						}
-						
-					} else if (seleccioEdicio == 2) {
-						
-					}
+
 					
+							 Element element0 = document.getDocumentElement();
+							 
+							 Element element1 = document.createElement("alumne");
+							 element1.setAttribute("id", String.valueOf(contadorId));
+							 
+							 element0.appendChild(element1);
+							 
+							 Element nom = document.createElement("nom");
+							 System.out.println("Introduce el nombre");
+							 nom.appendChild(document.createTextNode(teclado.nextLine()));
+							 element1.appendChild(nom);
+							 
+							 Element cognom1 = document.createElement("cognom1");
+							 System.out.println("Introduce el cognom1");
+							 cognom1.appendChild(document.createTextNode(teclado.nextLine()));
+							 element1.appendChild(cognom1);
+							 
+							 Element cognom2 = document.createElement("cognom2");
+							 System.out.println("Introduce el cognom2");
+							 cognom2.appendChild(document.createTextNode(teclado.nextLine()));
+							 element1.appendChild(cognom2);
+							 
+							 Element notaFinal = document.createElement("notaFinal");
+							 System.out.println("Introduce la nota final");
+							 notaFinal.appendChild(document.createTextNode(teclado.nextLine()));
+							 element1.appendChild(notaFinal);
+							 
+							 contadorId++;
+
+							 
+						} else if (seleccioEdicio == 2) {
+
+
+						} else if (seleccioEdicio == 3) {
+
+
+						}
+
+					} else if (seleccioEdicio == 2) {
+
+						System.out.println("Introdueix la ID:");
+						int seleccioId = teclado.nextInt();
+						Element editElement = document.getElementById(String.valueOf(seleccioId));
+						System.out.println(String.valueOf(editElement));
+					}
+
 				} else if (seleccio == 0) {
+
+					Transformer transformer = TransformerFactory.newInstance().newTransformer();
+					Result output = new StreamResult(new File("alumnesResultat.xml"));
+					Source input = new DOMSource(document);
+
+					transformer.transform(input, output);
 					
 					parar = true;
 				}
 			}
-			
+
 
 		} catch (Exception e) {
 			e.printStackTrace();
